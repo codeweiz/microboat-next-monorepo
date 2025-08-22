689d890359f829c8cf7ca21b4b10707d6d4b8618
codeweiz <13955645241@163.com>
2025-08-22 19:35:19 +0800

    fix(i18n): 优化导入

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -55,4 +55,3 @@ export const config = {
 		"/((?!api|images|image-proxy|fonts|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.svg|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.gif|.*\\.webp|.*\\.ico).*)",
 	],
 };
-
